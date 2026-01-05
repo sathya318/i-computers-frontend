c7e5221dbb70a44f14dada0f7fe80ab5d7a41cf9
sathya318 <thamarasiwijayabandu@gmail.com>
2026-01-06 01:27:35 +0530

    fix frontend  css issue😊

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
 
 
-
 function App() {
-  
-
   return (
-    <div className = "w-[244] h-24 border bg-red-600" > 
-      
-      
-      
-      
-    </div>
+    
+      <div className="w-32 h-24 border bg-red-600 text-white flex items-center justify-center">
+        Tailwind is working
+      </div>
+    
   );
 }
 
-export default App
+export default App;
